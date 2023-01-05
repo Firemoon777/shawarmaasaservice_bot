33d2be29f71f8920904f95096abea3eb33a486bd
Vladimir Turov <firemoon@icloud.com>
2023-01-05 20:48:29 +0300

    more gift options

--- shaas_bot/handlers/dm/gift.py
+++ shaas_bot/handlers/dm/gift.py
@@ -1,13 +1,9 @@
-import datetime
-
-from sqlalchemy import select, distinct
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, Chat
+from telegram import Update
 from telegram.ext import CallbackContext, MessageHandler, filters, CommandHandler, ConversationHandler
 from telegram.helpers import mention_markdown
 
 from shaas_bot.utils import cancel_handler
-from shaas_common.action.admin import get_controlled_chats
-from shaas_common.model import Order, Event
+from shaas_common.model import Chat
 from shaas_common.storage import Storage
 
 
@@ -16,17 +12,43 @@ GIFT_COUNT = 2
 
 
 async def gift(update: Update, context: CallbackContext):
-    await update.message.reply_text("Введите ник человека, которому вы хотите отправить купон")
+    await update.message.reply_text(
+        "Введите ник человека, которому вы хотите отправить купон.\n"
+        "Если ника нет, то можно переслать сообщение. Если человек запретил пересылать свои сообщения, "
+        "то можно указать число - идентификатор в телеграме."
+    )
     return NICK
 
 
-async def gift_get_nick(update: Update, context: CallbackContext):
+async def gift_parse_nick(update: Update, context: CallbackContext):
     s = Storage()
 
-    username = update.message.text.replace("@", "")
+    username = update.message.text.replace("@", "").replace("https://t.me/", "")
 
     async with s:
-        user = await s.chat.get_by_username(username)
+        try:
+            user = await s.chat.get(int(username))
+        except ValueError:
+            user = await s.chat.get_by_username(username)
+
+    return await gift_user(update, context, user)
+
+
+async def gift_parse_forwarded(update: Update, context: CallbackContext):
+    s = Storage()
+
+    if not update.message.forward_from:
+        await update.message.reply_text("Пользователь запретил пересылать свои сообщения. Идентификация невозможна.")
+        return ConversationHandler.END
+    user_id = update.message.forward_from.id
+
+    async with s:
+        user = await s.chat.get(user_id)
+
+    return await gift_user(update, context, user)
+
+
+async def gift_user(update: Update, context: CallbackContext, user: Chat):
 
     if not user:
         await update.message.reply_text(
@@ -38,10 +60,15 @@ async def gift_get_nick(update: Update, context: CallbackContext):
     context.user_data["user_id"] = user.id
     context.user_data["fullname"] = user.name
 
+    s = Storage()
     async with s:
         total_coupons = await s.coupon.get_coupons(update.message.from_user.id, user.id)
 
-    await update.message.reply_text(f"Сколько купонов нужно отправить?\n\nСейчас купонов у пользователя: {total_coupons}")
+    mention = mention_markdown(user.id, user.name)
+    await update.message.reply_text(
+        f"Сколько купонов нужно отправить?\n\nСейчас купонов у пользователя {mention}: {total_coupons}",
+        parse_mode="markdown"
+    )
     return GIFT_COUNT
 
 
@@ -69,7 +96,7 @@ gift_handler = ConversationHandler(
         CommandHandler("gift", gift, filters.ChatType.PRIVATE),
     ],
     states={
-        NICK: [MessageHandler(filters.TEXT, gift_get_nick)],
+        NICK: [MessageHandler(filters.FORWARDED, gift_parse_forwarded), MessageHandler(filters.TEXT, gift_parse_nick)],
         GIFT_COUNT: [MessageHandler(filters.TEXT, gift_send)]
     },
     fallbacks=[cancel_handler]
